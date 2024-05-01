535f4d2218055f025b2282c5d4c1b96ab97a3be5
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2024-04-30 22:55:34 -0700

    rjberwf

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 4/23/2024 10:58:22 PM
+ * Author : Administrator
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
